844e3a61c5bd406e5f9f9f4c0f9ad447f813e751
carolkiarie <wairimucarolk@gmail.com>
2023-03-13 22:12:20 +0100

    css edits

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,27 @@ import '../header/Header.css'
 import Cta from '../cta/Cta'
 import Image from '../assets/main-image.png'
 import Socials from '../headerSocials/Socials'
+import Typed from "react-typed";
 
 export default function Header() {
+  const transition = { duration: 3, type: "spring" };
   return (
    <div id='header'>
     <div className='header_container'>
-    <h5>Hello I'm</h5>
-    <h1>Caroline Kiarie</h1>
-    <h5 className='text-light'>A FrontEnd Developer</h5>
+    <h2>Hello! I am</h2>
+    <h3>Caroline Kiarie</h3>
+    <span className='text-light'>
+            <Typed
+              strings={[
+                "A Frontend Developer keen on improving her skills.",
+                "A passionate Web designer with an extreme creative mind🎇",
+                "An enthusiastic Coder 🤓🥳😇",
+              ]}
+              typeSpeed={50}
+              backSpeed={20}
+              loop
+            />
+          </span>
     <Cta/>
     <Socials/>
     <div className='me'>
